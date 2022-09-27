7f927ffcb7e9bf932f7d9753b2c8baaf198c9722
JungwooSim <cscd053@gmail.com>
2022-09-27 21:06:23 +0900

    add : 수동 커밋 추가

--- kafka/src/main/kotlin/com/example/kafka/service/KafkaConsumer.kt
+++ kafka/src/main/kotlin/com/example/kafka/service/KafkaConsumer.kt
@@ -1,6 +1,7 @@
 package com.example.kafka.service
 
 import com.example.kafka.dto.DataDto
+import org.apache.kafka.clients.consumer.Consumer
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
@@ -13,9 +14,11 @@ class KafkaConsumer {
     groupId = "foo",
     containerFactory = "helloKafkaListenerContainerFactory"
   )
-  fun helloListener(dataDto: DataDto) {
+  fun helloListener(dataDto: DataDto, consumer: Consumer<String, String>) {
     println("----")
     println(dataDto.name)
     println(dataDto.age)
+
+    consumer.commitSync()
   }
 }
