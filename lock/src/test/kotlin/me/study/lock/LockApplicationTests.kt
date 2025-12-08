ffaf9fb410abd8e914aec4843545152c5591eb43
JungwooSim <cscd053@gmail.com>
2025-12-08 23:23:30 +0900

    lock test set

---
+++ lock/src/test/kotlin/me/study/lock/LockApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.study.lock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LockApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
